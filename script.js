ecc0528044979b7f06ece6e5def66e57c3232389
boubacargd <dev.boubacargadio@gmail.com>
2024-04-08 02:26:13 +0200

    pop up

---
+++ script.js
@@ -0,0 +1,43 @@
+function openPopUp1() {
+    document.getElementById("overlay1").style.display = "block";
+    document.getElementById("popUp1").style.display = "block";
+}
+function openPopUp2() {
+    document.getElementById("overlay2").style.display = "block";
+    document.getElementById("popUp2").style.display = "block";
+}
+function openPopUp3() {
+    document.getElementById("overlay3").style.display = "block";
+    document.getElementById("popUp3").style.display = "block";
+}
+function openPopUp4() {
+    document.getElementById("overlay4").style.display = "block";
+    document.getElementById("popUp4").style.display = "block";
+}
+function openPopUp5() {
+    document.getElementById("overlay5").style.display = "block";
+    document.getElementById("popUp5").style.display = "block";
+}
+
+
+function closePopUp1() {
+    document.getElementById("overlay1").style.display = "none";
+    document.getElementById("popUp1").style.display = "none";
+}
+
+function closePopUp2() {
+    document.getElementById("overlay2").style.display = "none";
+    document.getElementById("popUp2").style.display = "none";
+}
+function closePopUp3() {
+    document.getElementById("overlay3").style.display = "none";
+    document.getElementById("popUp3").style.display = "none";
+}
+function closePopUp4() {
+    document.getElementById("overlay4").style.display = "none";
+    document.getElementById("popUp4").style.display = "none";
+}
+function closePopUp5() {
+    document.getElementById("overlay5").style.display = "none";
+    document.getElementById("popUp5").style.display = "none";
+}
